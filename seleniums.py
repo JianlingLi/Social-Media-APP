ae2e9d9db226d0e67440215fe4b50596cf961061
JianlingLi <122969933+JianlingLi@users.noreply.github.com>
2023-06-03 11:52:48 +0200

    Update seleniums.py

--- seleniums.py
+++ seleniums.py
@@ -21,7 +21,7 @@ def prepare_browser():
     chrome_options.add_argument("start-maximized")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option('useAutomationExtension', False)
-    driver = webdriver.Chrome('C:/Users/Ling/Downloads/chromedriver_win32/chromedriver.exe')
+    driver = webdriver.Chrome('YOUR PATH OF CHROMEDRIVER/chromedriver.exe')
     driver.get("https://www.instagram.com/")
     stealth(driver,
             user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36',
